e6c645bb3b6a9cca9cbf49c017f86934388a396a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-16 18:55:29 -0500

    render like and dislike icons

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <img src = {require("../icons/x-icon.webp")} alt="" />
+        <img src = {require("../icons/heart-icon.png")} alt="" />
+    </div>
+  )
+}
