ae6a3020735b3faee843d6fbdaade1d1174a9ad4
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-17 17:32:35 -0500

    Conditionally render Liked or Nope icon in profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 export default function Profile({img, name, age, bio}) {
+
+  const stateVar = true;
+
+
   return (
     <div className='profile-container' style={{backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat'}}>
         {/* <img src={img} alt="" /> */}
         <div className='liked-box'>
-          <h1>LIKED</h1>
+          <img className='liked-nope-image' src={stateVar ? require('../icons/like-image.png'): require('../icons/nope-image.png')} />
         </div>
         <div className='profile-info'>
           <h3>{name}, {age}</h3>
