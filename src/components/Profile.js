39bac1749edb1f7a9d078b31fd526835ecc1161d
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-17 17:13:07 -0500

    Add liked box div to conditionally render like or dislike icons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,9 @@ export default function Profile({img, name, age, bio}) {
   return (
     <div className='profile-container' style={{backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat'}}>
         {/* <img src={img} alt="" /> */}
+        <div className='liked-box'>
+          <h1>LIKED</h1>
+        </div>
         <div className='profile-info'>
           <h3>{name}, {age}</h3>
           <h5>{bio}</h5>
