9ad5a1b13cbcba5201bec03e1afeb026e0947d17
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-17 00:08:36 -0500

    Modify data like hasBeenSwiped and hasBeenLiked when like is clicked

--- src/App.js
+++ src/App.js
@@ -9,19 +9,39 @@ function App() {
 
   const [dogData, setDogData] = React.useState(dogs);
 
+  console.table(dogData)
+
   const [dogIndex, setDogIndex] = React.useState(0);
 
-  function likeProfile () {
+  function likeProfile (id) {
     setDogIndex(prevIndex => prevIndex + 1)
+    const updatedDogData = dogData.map(dog => {
+      if(dog.id === id){
+        return {
+          ...dog,
+          hasBeenSwiped: true,
+          hasBeenLiked: true,
+        }
+      }
+      return dog
+    })
+
+    setDogData(updatedDogData)
   }
+
+
+
   function skipProfile () {
     setDogIndex(prevIndex => prevIndex + 1)
   }
+
+  console.log('id from app: ' + dogData[dogIndex].id)
+
   return (
     <div className="App">
       <Header />
       <Profile img={dogData[dogIndex].avatar} name={dogData[dogIndex].name} age={dogData[dogIndex].age} bio={dogData[dogIndex].bio}/>
-      <Footer likeProfile={likeProfile} skipProfile={skipProfile}/>
+      <Footer id={dogData[dogIndex].id} likeProfile={likeProfile} skipProfile={skipProfile}/>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function Footer({likeProfile, skipProfile}) {
+export default function Footer({likeProfile, skipProfile, id}) {
+  console.log('id from footer: ' + id)
   return (
     <div className='footer-container'>
-        <img src = {require("../icons/x-icon.webp")} alt="" onClick={skipProfile}/>
-        <img src = {require("../icons/heart-icon.png")} alt="" onClick={likeProfile}/>
+        <img src = {require("../icons/x-icon.webp")} alt="" onClick={() => skipProfile(id)}/>
+        <img src = {require("../icons/heart-icon.png")} alt="" onClick={() => likeProfile(id)}/>
     </div>
   )
 }
