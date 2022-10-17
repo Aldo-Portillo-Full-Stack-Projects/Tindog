d154d856a5587b58207c23bd049925b21c097637
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-16 23:41:54 -0500

    Create like profile and skip profile functions and pass them down to footer components buttons

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ import dogs from './data/data'
 
 function App() {
 
-  const [dogIndex, setDogIndex] = React.useState(2)
+  const [dogIndex, setDogIndex] = React.useState(0)
+
+  function likeProfile () {
+    setDogIndex(prevIndex => prevIndex + 1)
+  }
+  function skipProfile () {
+    setDogIndex(prevIndex => prevIndex + 1)
+  }
   return (
     <div className="App">
       <Header />
       <Profile img={dogs[dogIndex].avatar} name={dogs[dogIndex].name} age={dogs[dogIndex].age} bio={dogs[dogIndex].bio}/>
-      <Footer />
+      <Footer likeProfile={likeProfile} skipProfile={skipProfile}/>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Footer() {
+export default function Footer({likeProfile, skipProfile}) {
   return (
     <div className='footer-container'>
-        <img src = {require("../icons/x-icon.webp")} alt="" />
-        <img src = {require("../icons/heart-icon.png")} alt="" />
+        <img src = {require("../icons/x-icon.webp")} alt="" onClick={skipProfile}/>
+        <img src = {require("../icons/heart-icon.png")} alt="" onClick={likeProfile}/>
     </div>
   )
 }
