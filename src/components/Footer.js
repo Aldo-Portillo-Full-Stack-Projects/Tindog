2a59ba2d05adc9c54e4a0b753f837aeb27fbf0ab
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-17 17:27:09 -0500

    Add new icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer({decisionButton, id}) {
   
   return (
     <div className='footer-container'>
-        <img src = {require("../icons/x-icon.webp")} alt="" onClick={() => decisionButton(id, false)}/>
+        <img src = {require("../icons/x-icon.png")} alt="" onClick={() => decisionButton(id, false)}/>
         <img src = {require("../icons/heart-icon.png")} alt="" onClick={() => decisionButton(id, true)}/>
     </div>
   )
