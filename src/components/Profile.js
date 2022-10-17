6e1d13159a02ee1544d505166c1f4a9fabc16571
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-17 17:07:03 -0500

    Render image of dog in background css to hopefully conditionally render [yes] or [no] images over it

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function Profile({img, name, age, bio}) {
   return (
-    <div className='profile-container'>
-        <img src={img} alt="" />
-        <h3>{name}, {age}</h3>
-        <h5>{bio}</h5>
+    <div className='profile-container' style={{backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat'}}>
+        {/* <img src={img} alt="" /> */}
+        <div className='profile-info'>
+          <h3>{name}, {age}</h3>
+          <h5>{bio}</h5>
+        </div>
     </div>
     
   )
