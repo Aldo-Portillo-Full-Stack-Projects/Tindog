079329e6802a0b7399884d32d3c396497f9ea892
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-17 17:54:42 -0500

    Incorporate these functions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Footer({decisionButton, id}) {
+export default function Footer({ id, likeButton, nopeButton}) {
   
   return (
     <div className='footer-container'>
-        <img src = {require("../icons/x-icon.png")} alt="" onClick={() => decisionButton(id, false)}/>
-        <img src = {require("../icons/heart-icon.png")} alt="" onClick={() => decisionButton(id, true)}/>
+        <img src = {require("../icons/x-icon.png")} alt="" onClick={() => nopeButton(id)}/>
+        <img src = {require("../icons/heart-icon.png")} alt="" onClick={() => likeButton(id)}/>
     </div>
   )
 }
--- src/pages/SwipePage.js
+++ src/pages/SwipePage.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Profile from '../components/Profile'
 import Footer from '../components/Footer'
 
-export default function SwipePage({img, name, age, bio, id, decisionButton, displayIcon}) {
+export default function SwipePage({img, name, age, bio, id, decisionButton, displayIcon, likeButton, nopeButton}) {
   return (
     <div>
         <Profile img={img} name={name} age={age} bio={bio} displayIcon={displayIcon}/>
-        <Footer id={id} decisionButton={decisionButton}/>
+        <Footer id={id} likeButton={likeButton} nopeButton={nopeButton}/>
     </div>
     
   )
