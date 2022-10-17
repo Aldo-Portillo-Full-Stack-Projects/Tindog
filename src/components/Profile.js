b1bcd2b0d1968fd17f5878973ea490ebed00f137
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-17 16:16:04 -0500

    Fix styling for profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Profile({img, name, age, bio}) {
   return (
     <div className='profile-container'>
-        <h1>Profile</h1>
         <img src={img} alt="" />
         <h3>{name}, {age}</h3>
         <h5>{bio}</h5>
