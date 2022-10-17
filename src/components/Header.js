683c950508bf96cfaaedd1a252fddaa98cb9ea08
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-17 16:22:55 -0500

    Render icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <div className='header-container'>
-        <Link to="/"><h1>Profile</h1></Link>
-        <Link to="/liked">Liked</Link>
+        <img src={require('../icons/profile.png')} alt="" />
+        <Link to="/"><img src={require('../icons/paw.png')} alt="" /></Link>
+        <Link to="/liked"><img src={require('../icons/favorite.png')} alt="" /></Link>
     </div>
     
   )
