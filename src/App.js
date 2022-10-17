c6ab96b661c652c440d4d1a9c3b187a2c0ca0bf0
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-16 19:01:30 -0500

    Render profile component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Profile />
       <Footer />
     </div>
   );
