af8540d33eb83800671737dc8155556a7f32a3b0
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-16 23:23:51 -0500

    Add dog images and data file

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+const dogs = [
+    {
+        name: "Rex",
+        avatar: "./rex.webp",
+        age: 25,
+        bio: "Art. Literature. Natural wine. Yoga.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },{
+        name: "Bella",
+        avatar: "./bella.png",
+        age: 43,
+        bio: "Yup, that's my owner. U can meet him if you want",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Teddy",
+        avatar: "teddy.png",
+        age: 30,
+        bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    }
+]
+
+export default dogs
