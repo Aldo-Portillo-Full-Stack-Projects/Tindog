c955369cb5ba12e2f83bf52bc228a15a4b0f5a79
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-18 14:47:11 -0500

    Style Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default function Error() {
   return (
-    <div className='Error'>
+    <div className='error'>
         <h3>No more good boys in your area</h3>
         <p>Try again soon!</p>
-        <p>Click the paw to look at your favorite goodboys</p>
-        <Link to="/liked"><img src={require('../icons/dog-icon.png')} alt=""/></Link>
+        <p>Click the pup to look at your favorite goodboys</p>
+        <Link to="/liked"><img src={require('../icons/dog-icon.png')} alt="" className='error-image'/></Link>
     </div>
   )
 }
