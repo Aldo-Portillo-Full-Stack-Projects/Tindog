ab6c5e9eed706c9a3a3ef9bfaf1b4804cfc27093
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-27 18:48:10 -0600

    Create login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='login-page'>
+        <h1>Login</h1>
+        <form>
+            <label for="email">Email:</label>
+            <input type="email" required name="email" />
+            <label for="password">Password:</label>
+            <input type="password" required name="password" />
+            <button type='submit'>Login</button>
+        </form>
+        <h4>Dont have an account? <Link to="signup">Sign Up</Link></h4>
+    </div>
   )
 }
