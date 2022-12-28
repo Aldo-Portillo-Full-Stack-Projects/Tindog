acfb345e3ba4e63a4b922f0692c915492d4173c3
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-27 18:44:13 -0600

    Create basic homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='home-page'>
+        <h1>Welcome to Tindog</h1>
+        <h3>Meet your best friend</h3>
+        <Link to="/login"><button>Login</button></Link>
+        <Link to="/signup"><button>Sign Up</button></Link>
+    </div>
   )
 }
