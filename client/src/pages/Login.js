e343f5b587e509080474caa2359745e98f700518
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-29 13:01:06 -0600

    Link login page to backend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,15 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { loginUser, validateEmail} from '../services/authServices'
 
 export default function Login() {
+
+    const [formData, setFormData] = React.useState({
+        email: "",
+        password: "",
+      })
+    const {email, password} = formData
+
+      const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value})
+      }
+    
+      const login = async (e) => {
+        e.preventDefault();
+    
+        if( !email || !password){
+          console.log("All fields are required")
+        }
+    
+        if(!validateEmail(email)){
+          console.log("Please enter a valid email")
+        }
+    
+        const userData = { email, password
+        }
+    
+        try {
+          const data = await loginUser(userData)
+          console.log(data);
+        } catch (err) {
+          console.log(err)
+        }
+      } 
   return (
     <div className='login-page'>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={login}>
             <label for="email">Email:</label>
-            <input type="email" required name="email" />
+            <input type="email" required name="email" value={email} onChange={handleInputChange}/>
             <label for="password">Password:</label>
-            <input type="password" required name="password" />
+            <input type="password" required name="password" value={password} onChange={handleInputChange}/>
             <button type='submit'>Login</button>
         </form>
         <h4>Dont have an account? <Link to="/signup">Sign Up</Link></h4>
