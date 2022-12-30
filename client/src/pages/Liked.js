cdda1031eec70477aed143e989a912b03eb7990e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-30 13:52:13 -0600

    Get liked dogs from databse and render

--- client/src/pages/Liked.js
+++ client/src/pages/Liked.js
@@ -1,15 +1,38 @@
+import axios from 'axios';
 import React from 'react'
 import FavoritedDog from '../components/FavoritedDog'
+import { BACKEND_URL } from '../services/authServices'
 
-export default function liked({data}) {
+export default function Liked({data}) {
 
-    const filterFavorites = data.filter(dog => dog.hasBeenLiked === true)
+    const [likedDogs, setLikedDogs] = React.useState([])
 
-    const renderFavorites = filterFavorites.map(dog => <FavoritedDog name={dog.name} avatar={dog.avatar}/>)
+    React.useEffect(() => {
+      const getLikedDogs = async () => {
+        try{    
+            const response = await axios.get(`${BACKEND_URL}/api/user/getuser`);
+            setLikedDogs(response.data.likedDogs)
+            return response.data.likedDogs
+        } catch(err){
+            const message = (err.response && err.response.data && err.response.data.message)|| err.message || err.toString();
+            console.log(message)
+            
+        }
+      }
 
-    console.log(filterFavorites)
+      getLikedDogs()
+    }, [])
+
+    console.log(likedDogs)
+
+    const renderLikedDogs = likedDogs.map(dog => {
+      return <FavoritedDog name={dog.name} avatar={dog.avatar}/>
+    })
+    
   return (
-    <div className='liked-dogs'>{renderFavorites}</div>
+    <div className='liked-dogs'>
+      {renderLikedDogs}
+      </div>
 
   )
 }
