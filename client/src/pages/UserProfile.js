ac8607beeb8953b175c3d779497bb3082b364e08
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-30 14:28:00 -0600

    Use profile to test auth services

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,11 +1,37 @@
 import React from 'react'
-import { getUser, logoutUser } from '../services/authServices'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { SET_LOGIN } from '../redux/authSlice';
+import { getUser, logoutUser,getLoginStatus } from '../services/authServices'
 
 export default function UserProfile() {
-    console.log(getUser)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const logout = async () => {
+    await logoutUser();
+    console.log('User logged out')
+    await dispatch(SET_LOGIN(false))
+    navigate("/login")
+  }
+
+  const userData = async () => {
+    const user = await getUser();
+    console.log(user);
+  }
+  const userLoggedIn = async() => {
+    try{
+      const loginStatus = await getLoginStatus()
+      console.log(loginStatus)
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div className='profile-page'>
-        <button onClick={logoutUser}>Logout</button>
+        <button onClick={userLoggedIn}>Get Login Status</button>
+        <button onClick={userData}>Log User Data</button>
+        <button onClick={logout}>Logout</button>
     </div>
   )
 }
