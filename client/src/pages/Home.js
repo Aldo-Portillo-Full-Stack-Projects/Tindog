206847687b8dda2f3bda31f7996bf2ea9131bf95
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-30 14:58:03 -0600

    Style homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className='home-page'>
-        <h1>Welcome to Tindog</h1>
+    <div className='home-page login-register-page'>
+      <img src={require("../icons/firePaw.png")} alt=""/>
+        <h2>Welcome to Tindog</h2>
         <h3>Meet your best friend</h3>
-        <Link to="/login"><button>Login</button></Link>
-        <Link to="/signup"><button>Sign Up</button></Link>
+        <div className='home-page-buttons'>
+          <Link to="/login"><button>Login</button></Link>
+          <Link to="/signup"><button>Sign Up</button></Link>
+        </div>
+        
     </div>
   )
 }
